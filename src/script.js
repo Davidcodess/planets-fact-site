394d2c43f581605cf68a3cdd24208d9064364f1f
David Johnson <dmjohnson844@gmail.com>
2023-05-25 17:55:37 -0400

    using javascript to switch between planet data

--- src/script.js
+++ src/script.js
@@ -1,1 +1,55 @@
-document.getElementsByClassName('planet-internal-content')[0].style.visability= "hidden";
+
+
+const planetData = document.querySelectorAll('.planetFact')
+const overviewBtn = document.getElementById('overviewButton')
+const internalBtn = document.getElementById('internalButton')
+const geologyBtn = document.getElementById('geologyButton')
+const planetImg = document.querySelectorAll('.planetImg')
+
+
+planetData.forEach( planetFact => {
+    planetFact.style.display = "none"
+    planetImg[1].style.display = "none"
+    planetImg[2].style.display = "none"
+})
+
+
+function addActiveClass() {
+
+
+    overviewBtn.addEventListener('click',() => {
+        planetData[0].style.display = "block"
+        planetData[1].style.display = "none"
+        planetData[2].style.display = "none"
+
+        planetImg[0].style.display ="block"
+        planetImg[1].style.display ="none"
+        planetImg[2].style.display = "none"
+    
+    })
+
+    internalBtn.addEventListener('click',() => {
+
+        planetData[1].style.display = "block"
+        planetData[0].style.display = "none"
+        planetData[2].style.display = "none"
+
+        planetImg[1].style.display ="block"
+        planetImg[0].style.display ="none"
+
+    })
+
+    geologyBtn.addEventListener('click',() => {
+        planetData[0].style.display = "none"
+        planetData[1].style.display = "none"
+        planetData[2].style.display = "block"
+
+        planetImg[1].style.display ="block"
+        planetImg[0].style.display ="none"
+        planetImg[2].style.display ="block"
+
+
+    })
+
+};
+
